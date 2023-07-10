abbb44e649f7363580e9d2683be5833641759c62
tommyrunner <1223758238@qq.com>
2023-07-10 18:04:58 +0800

    feat: 优化utils，批量修改样式

--- uitls.ts
+++ uitls.ts
@@ -1,25 +1,24 @@
 type ElementStyle = {
-    position?: string
-    zIndex?: number
-    top?: string
-    left?: string
-    width?: string
-    height?: string
-    overflow?: string
-    margin?: string
-    opacity?: number
-    transition?: string
-}
+  position?: string;
+  zIndex?: number;
+  top?: string;
+  left?: string;
+  width?: string;
+  height?: string;
+  overflow?: string;
+  margin?: string;
+  opacity?: number;
+  transition?: string;
+};
 /**
  * Set style in batch
- * @param element 
- * @param style 
+ * @param element
+ * @param style
  */
 export function setElementStyle(element: HTMLElement | null, style: ElementStyle) {
-    Object.entries(style).forEach(s => {
-        if (element && element.style) {
-            let styles: { [key: string]: any } = element.style
-            styles[s[0]] = s[1]
-        }
-    })
+  let temStyle: { [key in string]: string | number } = {};
+  Object.entries(style).forEach((s) => {
+    temStyle[s[0]] = s[1];
+  });
+  if (element && element.style) Object.assign(element.style, temStyle);
 }
